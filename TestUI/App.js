6496a906385dfc131874d22ea7f86145a61036e5
TruyenLe <truyenle@TruyenLes-Mac-mini.local>
2021-06-27 19:36:38 +0700

    init commit

---
+++ TestUI/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return <View style={styles.container} />
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'pink'
+  }
+});
+
+export default App;
